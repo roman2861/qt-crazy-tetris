5cf2a6bf2f3c315628995c80a1ccdb9d2295f216
dinhkimngan.90@gmail.com <dinhkimngan.90@gmail.com@209f4544-40f6-5be9-f567-3e86397f2b73>
2012-04-12 13:40:54 +0000

    v1

---
+++ trunk/Tetris/qml/Tetris/Z_FIGURE.js
@@ -0,0 +1,42 @@
+.pragma library
+
+var maxOrientation = 2
+var maxCell = 4
+var orients = new Array(maxOrientation)
+orients[0] = [
+            {
+                "x": 0,
+                "y":  0
+            },
+            {
+                "x": 0,
+                "y": 1
+            },
+            {
+                "x":  -1,
+                "y":  1
+            },
+            {
+                "x": 1,
+                "y": 0
+            },
+        ];
+orients[1] = [
+            {
+                "x": 0,
+                "y":  0
+            },
+            {
+                "x": 0,
+                "y": 1
+            },
+            {
+                "x":  1,
+                "y":  1
+            },
+            {
+                "x": 1,
+                "y": 2
+            },
+        ];
+
